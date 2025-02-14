a3823998d29d08747cdd3b5f91f4669c4104e12c
dot-ruth <ruthgetaneh5@gmail.com>
2025-02-14 07:21:00 +0300

    feat:imported the gemini api

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
 
 void main() {
+  Gemini.init(
+    apiKey: dotenv.env['GEMINI_API_KEY'] ?? ''
+  );
   runApp(const MyApp());
 }
 
