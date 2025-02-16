3057d6343eb55354ae4a1121292f752096fba98f
dot-ruth <ruthgetaneh5@gmail.com>
2025-02-16 13:58:45 +0300

    feat: prompt suggestion, input validation, selectable text and removed user avatars

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -14,131 +14,191 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   ChatUser currentUser = ChatUser(id: "0", firstName: "You");
   ChatUser dot = ChatUser(
-      id: "1",
-      firstName: "Dot",
-      profileImage: 'assets/dot.png');
+    id: "1",
+    firstName: "Dot",
+  );
   List<ChatMessage> messages = [];
   final Gemini gemini = Gemini.instance;
   bool isBotTyping = false;
+  final TextEditingController _textController = TextEditingController();
+  final List<String> samplePrompts = [
+    "How are you?",
+    "Tell me a joke",
+  ];
 
- @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      backgroundColor: Colors.white,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Text(
-            "DOT",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ],
-      ),
-      leading: IconButton(
-          icon: Icon(Icons.history), 
-          onPressed: () {},
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children:  [
+            Row(
+              children: [
+                IconButton(
+                          icon: Icon(CupertinoIcons.line_horizontal_3_decrease),
+                          onPressed: () {},
+                        ),
+                Text(
+                  "DOT",
+                  style: TextStyle(fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+
+            Row(
+              children: [
+                IconButton(
+                icon: Icon(CupertinoIcons.chat_bubble),
+                onPressed: () {},
+                          ),
+                          IconButton(
+                icon: Icon(CupertinoIcons.delete),
+                onPressed: () {
+                  setState(() {
+                    messages.clear();
+                  });
+                },
+                          ),
+              ],
+            ),
+          ],
         ),
-      actions: [
-        IconButton(
-        icon: Icon(CupertinoIcons.chat_bubble), 
-        onPressed: () {}  ,
-      )
-      ],
-    ),
-    body: _buildChatUI(),
-  );
-}
+      ),
+      body: _buildChatUI(),
+    );
+  }
 
-
-Widget _buildChatUI() {
-  return Container(
-    color: Colors.white, 
-    child: Column(
-      children: [
-        if (messages.isEmpty)
+  Widget _buildChatUI() {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          if (messages.isEmpty)
+            Expanded(
+              child: Center(
+                child: Column(
+                  children: [
+                    Image.asset('assets/dot.png', width: 100, height: 100),
+                    const SizedBox(height: 5),
+                    const Text(
+                      "Welcome to Dot Chat!",
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 5),
+                    const Text(
+                      "Chat with Dot, your AI assistant.",
+                      style: TextStyle(fontSize: 12, color: Colors.grey),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 5),
+                    Column(
+                children: samplePrompts.map((prompt) {
+                  return GestureDetector(
+                    onTap: () {
+                      _textController.text = prompt;
+                      _textController.selection = TextSelection.fromPosition(
+                        TextPosition(offset: _textController.text.length),
+                      );
+                      setState(() {});
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+                      margin: EdgeInsets.only(bottom: 5),
+                      width: 200,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(color: Colors.grey.shade300),
+                      ),
+                      child: Text(
+                        prompt,
+                        style: const TextStyle(fontSize: 14),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+                  ],
+                ), 
+              ),
+            ),
           Expanded(
-            flex: 1,
-            child: Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset('assets/dot.png', width: 100, height: 100), 
-                  const SizedBox(height: 20),
-                  const Text(
-                    "Welcome to Dot Chat!",
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    "Chat with Dot, your AI assistant.",
-                    style: TextStyle(fontSize: 16, color: Colors.grey),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
+            child: DashChat(
+              currentUser: currentUser,
+              onSend: _sendMessage,
+              messages: messages,
+              typingUsers: isBotTyping ? [dot] : [],
+              inputOptions: InputOptions(
+                textController: _textController,
+                alwaysShowSend: true,
+                sendOnEnter: true,
+                inputDecoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+                  hintText: "Type a message...",
+                ),
+              ),
+              messageOptions: MessageOptions(
+                showOtherUsersAvatar: false,
+                messagePadding: const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+                messageTextBuilder: (ChatMessage message, ChatMessage? previousMessage, ChatMessage? nextMessage) {
+                  return MarkdownBody(
+                    data: message.text,
+                    selectable: true,
+                    styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
+                      p: TextStyle(
+                        color: message.user.id == currentUser.id ? Colors.white : Colors.black,
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
           ),
-        Expanded(
-          flex: 2,
-          child: DashChat(
-            currentUser: currentUser,
-            onSend: _sendMessage,
-            messages: messages,
-            typingUsers: isBotTyping ? [dot] : [],
-            inputOptions: InputOptions(
-              alwaysShowSend: true,
-              sendOnEnter: true,
-            ),
-            messageOptions: MessageOptions(
-              messageTextBuilder: (ChatMessage message, ChatMessage? previousMessage, ChatMessage? nextMessage) {
-                return MarkdownBody(
-                  data: message.text,
-                  styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
-                    p: TextStyle(
-                      color: message.user.id == currentUser.id ? Colors.white : Colors.black,
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ),
-      ],
-    ),
-  );
-}
+        ],
+      ),
+    );
+  }
 
   void _sendMessage(ChatMessage chatMessage) {
-    setState(() {
-      messages.insert(0, chatMessage);
-      isBotTyping = true;
-    });
+    if (chatMessage.text.trim().isNotEmpty) {
+      setState(() {
+        messages.insert(0, chatMessage);
+        isBotTyping = true;
+      });
 
       List<Part> parts = [TextPart(chatMessage.text)];
       final responseStream = gemini.promptStream(parts: parts);
 
-      responseStream.listen((event) {
-        if (messages.first.user.id == dot.id) {
+      responseStream.listen(
+        (event) {
+          if (messages.first.user.id == dot.id) {
+            setState(() {
+              messages.first.text += event!.content!.parts!.map((part) => (part as TextPart).text).join(' ');
+            });
+          } else {
+            setState(() {
+              messages.insert(0, ChatMessage(
+                text: event!.content!.parts!.map((part) => (part as TextPart).text).join(' '),
+                user: dot,
+                createdAt: DateTime.now(),
+              ));
+            });
+          }
+        },
+        onDone: () {
           setState(() {
-             messages.first.text += event!.content!.parts!.map((part) => (part as TextPart).text).join(' '); 
+            isBotTyping = false;
           });
-        } else {
-          setState(() {
-            messages.insert(0, ChatMessage(
-              text: event!.content!.parts!.map((part) => (part as TextPart).text).join(' '),
-              user: dot,
-              createdAt: DateTime.now(),
-            ));
-          });
-        }
-      },
-      onDone:  () {
-        setState(() {
-          isBotTyping = false;
-        });
-      }
+        },
       );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Message cannot be empty')),
+      );
+    }
   }
-
 }
